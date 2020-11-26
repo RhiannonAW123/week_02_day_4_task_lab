543cd3beb8cdbdcc662f288129915eae17968d7e
Simon Charters <simon259@hotmail.co.uk>
2020-11-26 15:19:59 +0000

    Extension complete

--- src/task_decider.py
+++ src/task_decider.py
@@ -2,23 +2,45 @@ from src.task import Task
 
 def task_decider(task_1, task_2):
     if task_1.description == "Clean Windows" or task_2.description == "Clean Windows":
-        if task_1.description == "Wash Dishes" or task_2.description == "Wash Dishes":
-            return "Clean Windows"
-        else:
+        if task_1.description == "Wash Clothes" or task_2.description == "Wash Clothes":
+            return "Wash Clothes"
+        elif task_1.description == "Cook Dinner" or task_2.description == "Cook Dinner":
             return "Cook Dinner"
+        else:
+            return "Clean Windows"
     
     elif task_1.description ==  "Wash Dishes" or task_2.description == "Wash Dishes":
-        if task_1.description == "Cook Dinner" or task_2.description == "Cook Dinner":
-            return "Wash Dishes"
-        else: 
+        if task_1.description == "Do Ironing" or task_2.description == "Do Ironing":
+            return "Do Ironing"
+        elif task_1.description == "Clean Windows" or task_2.description == "Clean Windows":
             return "Clean Windows"
+        else: 
+            return "Wash Dishes"
 
     
     elif task_1.description ==  "Cook Dinner" or task_2.description == "Cook Dinner":
-        if task_1.description == "Clean Windows" or task_2.description == "Clean Windows":
-            return "Cook Dinner"
-        else: 
+        if task_1.description == "Wash Dishes" or task_2.description == "Wash Dishes":
             return "Wash Dishes"
+        elif task_1.description == "Wash Clothes" or task_2.description == "Wash Clothes":
+            return "Wash Clothes"
+        else: 
+            return "Cook Dinner"
+
+    if task_1.description == "Do Ironing" or task_2.description == "Do Ironing":
+        if task_1.description == "Cook Dinner" or task_2.description == "Cook Dinner":
+            return "Cook Dinner"
+        elif task_1.description == "Clean Windows" or task_2.description == "Clean Windows":
+            return "Clean Windows"
+        else:
+            return "Do Ironing"
+
+    if task_1.description == "Wash Clothes" or task_2.description == "Wash Clothes":
+        if task_1.description == "Wash Dishes" or task_2.description == "Wash Dishes":
+            return "Wash Dishes"
+        elif task_1.description == "Do Ironing" or task_2.description == "Do Ironing":
+            return "Do Ironing"
+        else:
+            return "Wash Clothes"
 
 
     
--- tests/test_task.py
+++ tests/test_task.py
@@ -7,6 +7,8 @@ class TestTask(unittest.TestCase):
         self.task_1 = Task("Wash Dishes", 20)
         self.task_2 = Task("Cook Dinner", 45)
         self.task_3 = Task("Clean Windows", 40)
+        self.task_4 = Task("Do Ironing", 40)
+        self.task_5 = Task("Wash Clothes", 60)
 
     def test_task_desription(self):
         self.assertEqual("Wash Dishes", self.task_1.description)
@@ -25,5 +27,12 @@ class TestTask(unittest.TestCase):
 
     def test_task_decider__returns_clean_windows_reversed(self):
         self.assertEqual("Clean Windows", task_decider(self.task_1, self.task_3))
+    
+    def test_task_decider__wash_dishes_wash_clothes(self):
+        self.assertEqual("Wash Dishes", task_decider(self.task_1, self.task_5))
+
+    def test_task_decider__wash_clothes_cook_dinner(self):
+        self.assertEqual("Wash Clothes", task_decider(self.task_5, self.task_2))
+    
 
     
