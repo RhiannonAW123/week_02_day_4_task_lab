f4a38908ddd46cff23f3b78c3bea72946d37083b
Simon Charters <simon259@hotmail.co.uk>
2020-11-26 13:54:16 +0000

    setup complete

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest 
+
+from src.test_task import TestTask
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_task.py
@@ -0,0 +1,6 @@
+import unittest
+
+from src.task import Task
+
+class TestTask(unittest.TestCase):
+    pass
